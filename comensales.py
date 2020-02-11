33bf1d758652cf5937f98bc9b5311eea9c219df2
marianraven <mariansdh@gmail.com>
2020-02-11 20:48:45 -0300

    "arreglar comensal"

--- comensales.py
+++ comensales.py
@@ -3,18 +3,24 @@ import time
 import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
-
+semaphore = threading.Semaphore(platosDisponibles)
 class Cocinero(threading.Thread):
+   
+  
   def __init__(self):
     super().__init__()
     self.name = 'Cocinero'
 
   def run(self):
+    global semaphore
     global platosDisponibles
+    
     while (True):
-      logging.info('Reponiendo los platos...')
-      platosDisponibles = 3
-
+     
+      with semaphore:
+        logging.info('Reponiendo los platos...')
+        platosDisponibles = 3
+      
 class Comensal(threading.Thread):
   def __init__(self, numero):
     super().__init__()
@@ -22,8 +28,10 @@ class Comensal(threading.Thread):
 
   def run(self):
     global platosDisponibles
+    global semaphore
     platosDisponibles -= 1
     logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
+    
 
 platosDisponibles = 3
 
@@ -31,4 +39,5 @@ Cocinero().start()
 
 for i in range(5):
   Comensal(i).start()
+ 
 
