2016eaf00a7254d1c46c02d84337a304928c5a31
marianraven <mariangsdh@gmail.com>
2020-02-13 20:37:06 -0300

    "funciona , gracias edu"

--- comensales.py
+++ comensales.py
@@ -3,9 +3,11 @@ import time
 import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
-platosDisponibles = 3
-semaphore = threading.Semaphore(platosDisponibles)
-gateway = threading.Semaphore(1)
+
+semaCocinero = threading.Semaphore(0)
+semaComensal = threading.Semaphore(1)
+
+
 class Cocinero(threading.Thread):
   def __init__(self):
     super().__init__()
@@ -13,41 +15,44 @@ class Cocinero(threading.Thread):
 
   def run(self):
     global platosDisponibles
-    global semaphore
-    global gateway
-    while (True): 
-        gateway.acquire()
-        logging.info('Reponiendo los platos...')
-        platosDisponibles = 3
-        time.sleep(3)#sleep para poder hacer seguimiento del bucle mientras lo corrijo
-        semaphore.release()
+    global semaCocinero
+    global semaComensal
+    while (True):   
+      semaCocinero.acquire()
+      logging.info('Reponiendo los platos...')
+      platosDisponibles = 3
+      logging.info('A comerlaaaaa...')
+      semaComensal.release()
       
+
 class Comensal(threading.Thread):
   def __init__(self, numero):
     super().__init__()
-    self.name = f'Comensal {numero}'
+    self.name = (f'Comensal {numero}')
 
   def run(self):
     global platosDisponibles
-    global semaphore
-    global gate
-    semaphore.acquire()
+    global semaCocinero
+    global semaComensal
+    
+    semaComensal.acquire()
     platosDisponibles -= 1
-    logging.info(f'¡Qué ricooooooooooooooooooooooo! Quedan {platosDisponibles} platos')
-    if platosDisponibles <= 1:
-      gateway.acquire()
-     
+    logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
+    
+    if platosDisponibles == 0:
+      logging.info('Cocinero Reponela')
+      semaCocinero.release()
+    else:
+      logging.info('A comerlaaaaa...')
+      semaComensal.release()
+      
+
+platosDisponibles = 3
+
 
 Cocinero().start()
 
-for i in range(5):
-    gateway.release()
-    logging.info(f'Quedan {platosDisponibles} platos')
+for i in range(10):
     Comensal(i).start()
 
- # al correr el código se ejecuta  cocinero, rango, comenzal(0),rango, comensal(1)
- # rango, comensal(2), rango, rango ,cocinero  , comensal(3), cocinero, comensal(4)
- # cocinero, cocinero....... 
- # Logre cortar el bucle infinito, logre que no me arroje platos negativos,
- # pero no encuentro el error y no estoy conforme.
-
+  
